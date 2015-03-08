7ae8dec24bd3eeb84c28948502d99fd3a8dee366
Hrovje Baic <baic989@gmail.com>
2015-03-08 17:50:55 +0100

    Setup four basic views

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
     var secondContainer: UIView!
     var thirdContainer: UIView!
     var fourthContainer: UIView!
+    
     let marginForView: CGFloat = 10.0
     let oneSixth: CGFloat = 1.0/6.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupContainerViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,8 +34,19 @@ class ViewController: UIViewController {
     func setupContainerViews() {
         self.firstContainer = UIView(frame: CGRectMake(self.view.bounds.origin.x + marginForView, self.view.bounds.origin.y, self.view.bounds.width - (marginForView * 2), self.view.bounds.height * oneSixth))
         self.firstContainer.backgroundColor = UIColor.redColor()
-        
         self.view.addSubview(self.firstContainer)
+        
+        self.secondContainer = UIView(frame: CGRectMake(self.view.bounds.origin.x + marginForView, firstContainer.frame.height, self.view.bounds.width - (marginForView * 2), self.view.bounds.height * (oneSixth * 3)))
+        self.secondContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(self.secondContainer)
+        
+        self.thirdContainer = UIView(frame: CGRectMake(self.view.bounds.origin.x + marginForView, firstContainer.frame.height + secondContainer.frame.height, self.view.bounds.width - (marginForView * 2), self.view.bounds.height * oneSixth))
+        self.thirdContainer.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.thirdContainer)
+        
+        self.fourthContainer = UIView(frame: CGRectMake(self.view.bounds.origin.x + marginForView, firstContainer.frame.height + secondContainer.frame.height + thirdContainer.frame.height, self.view.bounds.width - (marginForView * 2), self.view.bounds.height * oneSixth))
+        self.fourthContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(self.fourthContainer)
     }
 }
 
