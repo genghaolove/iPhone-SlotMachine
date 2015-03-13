e1da3ca3d80772c8b970d2767aea12c94d0cac70
Hrovje Baic <baic989@gmail.com>
2015-03-13 23:20:16 +0100

    Added all the image assets.

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Hrvoje Baic on 09/03/15.
+//  Copyright (c) 2015 Hrvoje Baic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Slot {
+    var value = 0
+    var image:UIImage! = UIImage(named: "Ace")
+    var isRed = true
+}
